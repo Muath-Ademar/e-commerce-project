ec9b032cec6b798df135a25afb4f182281c819f0
Muath-Ademar <muazademar20@gmail.com>
2025-07-25 12:21:10 +0300

    made a change to triggier a redeploy on render

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const path = require('path'); // Add this at the top with other requires
+const path = require('path'); // Add this at the top with other requires.
 const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
